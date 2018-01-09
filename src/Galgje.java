ddb2f8c5bbde29bb51adb89e8aacc697ff7ec15c
Koen Alblas <koen.alblas@gmail.com>
2018-01-09 13:39:09 +0100

    ble

--- src/Galgje.java
+++ src/Galgje.java
@@ -1,6 +1,11 @@
 
 public class Galgje {
 	public static void main (String args[] ) {
+		System.out.println("Welkom bij Galgje");
+		System.out.println("Het te raden woord");
 		
+		String teRadenWoord  = "Qien";
+		
+		System.out.println(teRadenWoord);
 	}
 }
