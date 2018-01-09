32fc5daa387315fd4541ab47c5f7db509673aafd
Koen Alblas <koen.alblas@gmail.com>
2018-01-09 14:38:05 +0100

    mwa

--- src/Galgje.java
+++ src/Galgje.java
@@ -1,28 +1,32 @@
 import java.util.Scanner;
 
 public class Galgje {
-	public static void main (String args[] ) {
+	public static String teRadenWoord = "NogNietsIngevuld";
+	public static void main(String args[]) {
 		System.out.println("Welkom bij Galgje");
 		System.out.println("Het te raden woord");
-		
-		String teRadenWoord  = "Qien";
-		
+
+		String teRadenWoord = "Qien";
+
 		System.out.println(teRadenWoord);
 		Scanner sc = new Scanner(System.in);
-		String i = sc.nextLine();
-		System.out.println(i);
-		
-		if(teRadenWoord.contains(i)) {
-			System.out.println("ja");
-		}else {
-			System.out.println("nee");
+
+		for (int j = 0; j < 10; j++) {
+			String i = sc.nextLine();
+			checkenOfLetterBestaat(i);
+
 		}
-	
-		
-		
-		
-		
-		
+
 		System.out.println("Het spel is klaar");
+	}
+
+	public static void checkenOfLetterBestaat(String i2) {
+		System.out.println("Uw ingevoerde letter:" + " " + i2);
+		if (teRadenWoord.contains(i2)) {
+			System.out.println("ja, komt voor");
+		} else {
+			System.out.println("nee, komt niet voor");
+
+		}
 	}
 }
